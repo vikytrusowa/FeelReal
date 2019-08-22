bda3ae625675d2baa9004254db355b57402d0590
vikytrusowa <vikytrusowa.@gmail.com>
2019-08-22 18:48:37 +0300

    completion and adaptation of the tracks page

--- js/main.js
+++ js/main.js
@@ -1,12 +1,6 @@
-function t01() {
-    var radio = document.getElementById("i01");
-    var div = document.getElementById("t01");
-    var div2 = document.getElementById("t02");
-
-    if (radio.checked) {
-        div.style.display = "inline";
-        div2.style.display = "none";
-        div_show = true;
-    }
-}
-var div_show = false;
+$('.sidebar__menu').on("click", function () {
+    $(".sidebar__list").addClass('sidebar__list--open')
+});
+$('.sidebar__close').on("click", function () {
+    $(".sidebar__list").removeClass('sidebar__list--open')
+});
