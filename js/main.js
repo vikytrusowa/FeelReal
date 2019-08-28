9dd9399c08b714aee1f83cdbc9d127f80397bf27
vikytrusowa <vikytrusowa.@gmail.com>
2019-08-28 12:47:18 +0300

    multiselect change

--- js/main.js
+++ js/main.js
@@ -4,3 +4,15 @@ $('.sidebar__menu').on("click", function () {
 $('.sidebar__close').on("click", function () {
     $(".sidebar__list").removeClass('sidebar__list--open')
 });
+var expanded = false;
+
+function showCheckboxes() {
+    var checkboxes = document.getElementById("checkboxes");
+    if (!expanded) {
+        checkboxes.style.display = "block";
+        expanded = true;
+    } else {
+        checkboxes.style.display = "none";
+        expanded = false;
+    }
+}
